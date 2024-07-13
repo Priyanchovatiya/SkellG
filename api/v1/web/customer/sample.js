17c1e03fbe0b576f5de2d626cea55c10ad118d9e
priyan chovatiya <priyanchovatiya3226@gmail.com>
2024-07-13 12:33:38 +0530

    vercel setting 2

--- api/v1/web/customer/sample.js
+++ api/v1/web/customer/sample.js
@@ -4,8 +4,13 @@ import fs from 'fs';
 const router = express.Router();
 
 
-router.get('/sam', 
- async (req, res) => {
-    console.log("hello from server");
- })
+router.get('/sam', async (req, res) => {
+   try {
+       console.log("Hello from server");
+       res.status(200).send("Hello from server");
+   } catch (error) {
+       console.error(error);
+       res.status(500).send("Internal Server Error");
+   }
+});
  export default router;
